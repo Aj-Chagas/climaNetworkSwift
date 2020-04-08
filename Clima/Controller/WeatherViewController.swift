da809d35b51de55fe10f0c42827f114e538cc268
andersonchagas <anderson.1997reis@gmail.com>
2020-04-08 19:01:09 -0300

    UITextFieldDelegate

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,45 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        searchTextField.delegate = self
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        print(searchTextField.text!)
+    }
+    
+    //dispara quado o botao retorno do teclado é acionado
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //fecha teclado
+        textField.endEditing(true)
+        return true
+    }
+    
+    // quando o usuario toca em outra coisa
+    // caso a string seja vazia, ele não consegue tirar o foco do text view
+   func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+      if textField.text != ""{
+          return true
+       } else{
+           textField.placeholder = "Type something"
+            return false
+       }
+    }
+    
+    //quando perde o foco
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.text = ""
+    }
 }
 
